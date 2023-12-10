6e3e8830de899ae6767065209207093fec6802ac
zahaale20 <alexcz@outlook.com>
2023-12-10 10:29:20 -0800

    requirement 3 and 4

--- httpd.c
+++ httpd.c
@@ -3,28 +3,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <errno.h>    
+#include <sys/stat.h>
 
-#define PORT 6969
+// requirement 4: error responses
+void send_error_response(FILE *network, const char *status, const char *message) {
+   fprintf(network, "%s\r\n", status);
+   fprintf(network, "Content-Type: text/html\r\n\r\n");
+   fprintf(network, "<html><body><h1>%s</h1></body></html>\r\n", message);
+}
 
+// requirement 3: support for head and get requests
 void handle_request(int nfd){
+   
+   // open file descriptor
    FILE *network = fdopen(nfd, "r+");
    char *line = NULL;
-   size_t size;
+   size_t size = 0;
    ssize_t num;
 
+   // error handling
    if (network == NULL){
-      perror("fdopen");
+      perror("Error: failed to open file descriptor.");
       close(nfd);
       return;
    }
+   
+   // loop to handle multiple requests in a single connection
+   while (1) {  
+      // read the request line from client
+      num = getline(&line, &size, network);
+      //check for read errors
+      if (num <= 0) {
+         if (feof(network)) {  // check for end of file (connection closed by client)
+            break;
+         }
+         // send a 400 Bad Request error in case of a read error
+         send_error_response(network, "HTTP/1.0 400 Bad Request", "400 Bad Request");
+         continue;  // continue to the next request instead of returning
+      }
 
-   while ((num = getline(&line, &size, network)) >= 0){
-      // printf("Received from client: %s", line);
-      write(nfd, line, num);
+      // parse the request line
+      char *method = strtok(line, " ");
+      char *path = strtok(NULL, " ");
+      if (!method || !path) { // check if successfully parsed
+         send_error_response(network, "HTTP/1.0 400 Bad Request", "400 Bad Request");
+         free(line);
+         fclose(network);
+         return;
+      }
+
+      // remove the leading '/' from the path
+      if (path[0] == '/') path++;
+
+      // check if requested file exists
+      struct stat file_stat;
+      int file_exists = stat(path, &file_stat);
+      if (file_exists < 0) {
+         send_error_response(network, "HTTP/1.0 404 Not Found", "404 Not Found");
+      } else {
+         // open requested file
+         FILE *file = fopen(path, "r");
+         if (file == NULL) { // handle file access permission error
+               send_error_response(network, "HTTP/1.0 403 Permission Denied", "403 Permission Denied");
+         } else { // else ok... send a 200 ok response header
+            fprintf(network, "HTTP/1.0 200 OK\r\n");
+            fprintf(network, "Content-Type: text/html\r\n");
+            fprintf(network, "Content-Length: %ld\r\n\r\n", file_stat.st_size);
+
+            // if get... send file content
+            if (strcmp(method, "GET") == 0) {
+               char buffer[1024];
+               while (fgets(buffer, sizeof(buffer), file)) {
+                  fprintf(network, "%s", buffer);
+               }
+            }
+            fclose(file); // close file
+         }
+      }
+      // free line buffer/reset
+      free(line);
+      line = NULL;
+      size = 0;
    }
-
-   free(line);
-   fclose(network);
+   // free memory and close the network stream
+    free(line);
+    fclose(network);
 }
 
 // requirement 2: handling multiple requests with child processes
